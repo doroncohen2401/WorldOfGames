ac2b683657131731285772000165d3b1dc63642e
Cohen, Doron <doron_cohen2@bmc.com>
2022-04-17 18:05:31 +0300

    s

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -1,29 +1,11 @@
-import requests
+# import requests
+from currency_converter import CurrencyConverter
 from GuessGame import generate_number
 
 
-class RealTimeCurrencyConverter():
-    def __init__(self, url):
-        self.data = requests.get(url).json()
-        self.currencies = self.data['rates']
-
-    def convert(self, from_currency, to_currency, amount):
-        initial_amount = amount
-        # first convert it into USD if it is not in USD.
-        # because our base currency is USD
-        if from_currency != 'USD':
-            amount = amount / self.currencies[from_currency]
-
-            # limiting the precision to 4 decimal places
-        amount = round(amount * self.currencies[to_currency], 4)
-        return amount
-
-
-def gen_currency_rate(num):
-    url_api = 'https://api.exchangerate-api.com/v4/latest/USD'
-    converter = RealTimeCurrencyConverter(url_api)
-    currency = converter.convert('USD', 'ILS', num)
-    return currency
+def gen_currency_converter(from_currency, to_currency, amount):
+    c = CurrencyConverter()
+    return c.convert(amount, from_currency, to_currency)
 
 
 def get_money_interval(total, difficult):
@@ -44,8 +26,8 @@ def get_guess_from_user(usa_dollar):
 
 def play(difficult):
     random_number = generate_number(101)
-    current_exchange_rate = gen_currency_rate(random_number)
-    money_range = get_money_interval(current_exchange_rate, difficult)
+    currency_converter = gen_currency_converter("USD", "ILS", float(random_number))
+    money_range = get_money_interval(currency_converter, difficult)
     guess_number = get_guess_from_user(random_number)
     condition = money_range[0] <= guess_number <= money_range[1]
     if condition:
